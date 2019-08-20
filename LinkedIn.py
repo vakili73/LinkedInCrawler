5a9799af9c88b4e9038edbe2fe6094bc1d8cf5d9
Vahid Vakili <v.vakili73@gmail.com>
2019-08-20 11:28:33 +0430

    first commit...

--- LinkedIn.py
+++ LinkedIn.py
@@ -1,26 +1,108 @@
 from selenium import webdriver
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class State:
+    INIT = 'init'
+    MAIN = 'main_page'
+    HOME = 'homepage_basic'
+
 
 class LinkedIn:
-    def __init__(self, driver=webdriver.Firefox()):
+    def __init__(self,
+                 state=State.INIT,
+                 driver=webdriver.Firefox()):
         self.__driver = driver
-        self.__is_link_opened = False
+        self.__state = state
 
-    def openLink(self, link='https://www.linkedin.com/'):
-        self.__driver.get(link)
+    def state(self):
+        return self.__state
 
-    def login(self, email, password):
-        if not self.__is_link_opened:
-            self.openLink()
-        
-        pass
+    def driver(self):
+        return self.__driver
+
+    def open(self):
+        assert self.__state == State.INIT, 'To open the LinkedIn website your state must be in the State.INIT.'
+
+        try:
+            self.__driver.get('https://www.linkedin.com/')
+            self.__state = State.MAIN
+            return True
+
+        except Exception as error:
+            self.__driver.close()
+            raise error
+
+    def login(self, username, password):
+        assert self.__state == State.MAIN, 'To login to the LinkedIn website your state must be in the State.MAIN.'
+
+        try:
+            inputUsername = self.__driver.find_element(By.XPATH,
+                                                       value='/html/body/nav/section[2]/form/div[1]/div[1]/input')
+            inputUsername.send_keys(username)
+
+            inputPassword = self.__driver.find_element(By.XPATH,
+                                                       value='/html/body/nav/section[2]/form/div[1]/div[2]/input')
+            inputPassword.send_keys(password)
+
+            buttonSignIn = self.__driver.find_element(By.XPATH,
+                                                      value='/html/body/nav/section[2]/form/div[2]/button')
+            buttonSignIn.click()
+
+            self.__state = State.HOME
+            return True
+
+        except Exception as error:
+            self.__driver.close()
+            raise error
+
+    def logout(self):
+        assert not (self.__state == State.INIT or self.__state ==
+                    State.MAIN), 'Can\'t logout before logged in'
+
+        try:
+            self.__driver.get('https://www.linkedin.com/m/logout/')
+            self.__state = State.INIT
+            self.open()
+            return True
+
+        except Exception as error:
+            self.__driver.close()
+            raise error
+
+    def waitUntil(self, timeout=10,
+                  condition=EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'footer'))):
+        try:
+            WebDriverWait(self.__driver, timeout).until(condition)
+            return True
+
+        except Exception as error:
+            self.__driver.close()
+            raise error
 
     def setProxy(self):
         pass
 
-    def getPerson(self):
+    def getProfile(self):
+        pass
+
+    def getJobs(self):
+        pass
+
+    def getFriends(self):
+        pass
+
+    def goto(self, state):
         pass
 
 
 if __name__ == "__main__":
+
     linkedin = LinkedIn()
+    linkedin.open()
+
+    linkedin.login('v.vakili73@gmail.com', '0307019580003')
+    linkedin.logout()
